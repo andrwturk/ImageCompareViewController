d15a771eedec856e40c8e277e24f98f357dc96b3
Andrew Turkin <andrew_turkin@Andrews-MacBook-Pro.local>
2015-03-01 10:34:31 +0200

    first commit

---
+++ ImageCompareViewController/ImageCompareViewController/CompareView.swift
@@ -0,0 +1,62 @@
+//
+//  CompareView.swift
+//  ImageCompareViewController
+//
+//  Created by Andrew Turkin on 2/26/15.
+//  Copyright (c) 2015 Andrew Turkin. All rights reserved.
+//
+
+import UIKit
+
+class CompareView: UIView {
+    
+    var leftOffset:CGFloat = 0.0 
+    var rightOffset:CGFloat = 0.0
+    var image:UIImage?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clearColor()
+        contentMode = UIViewContentMode.Redraw
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    override func drawRect(rect: CGRect) {
+        
+        if let anImage = image {
+            
+            let offsetRect = CGRectMake(bounds.origin.x + leftOffset, bounds.origin.y, bounds.size.width - rightOffset - leftOffset, bounds.size.height)
+            
+            if offsetRect.size.width > 0 {
+                let scaledRect = CGRectMake(offsetRect.origin.x * anImage.scale, offsetRect.origin.y * anImage.scale, offsetRect.size.width * anImage.scale, offsetRect.size.height * anImage.scale)
+                
+                
+                let imageRef = CGImageCreateWithImageInRect(anImage.CGImage, scaledRect)
+                
+                if let imageResult = UIImage(CGImage: imageRef, scale: anImage.scale, orientation: anImage.imageOrientation) {
+                    imageResult.drawInRect(offsetRect)
+                    let ctx = UIGraphicsGetCurrentContext()
+                    CGContextSetStrokeColorWithColor(ctx, UIColor.redColor().CGColor)
+                    
+                    if rightOffset != 0 {
+                        CGContextMoveToPoint(ctx, offsetRect.size.width, 0)
+                        CGContextAddLineToPoint(ctx, offsetRect.size.width, offsetRect.size.height)
+                    }
+                    
+                    if leftOffset != 0 {
+                        CGContextMoveToPoint(ctx, leftOffset, 0)
+                        CGContextAddLineToPoint(ctx, leftOffset, offsetRect.size.height)
+                    }
+                    CGContextStrokePath(ctx)
+                }
+            }
+            
+            
+        }
+        
+    }
+}
---
+++ ImageCompareViewController/ImageCompareViewController/ImageCompareViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  ImageCompareViewController
+//
+//  Created by Andrew Turkin on 2/26/15.
+//  Copyright (c) 2015 Andrew Turkin. All rights reserved.
+//
+
+import UIKit
+
+class ImageCompareViewController: UIViewController, UIScrollViewDelegate {
+    
+    var images = ["carbon.jpg", "classique.jpg", "gold.jpg", "minimal.jpg"]
+    
+    var backgroundView:UIImageView!
+    var foregroundView:CompareView!
+    
+    var backgroundScrollView:UIScrollView!
+    var scrollView:UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blackColor()
+        backgroundScrollView = UIScrollView(frame: view.bounds)
+        backgroundScrollView.contentSize = view.bounds.size
+        backgroundScrollView.alwaysBounceHorizontal = true
+        backgroundScrollView.delegate = self
+        backgroundScrollView.directionalLockEnabled = true
+        view.addSubview(backgroundScrollView)
+
+        backgroundView = UIImageView(frame: view.bounds)
+        backgroundView.contentMode = UIViewContentMode.Top
+        backgroundScrollView.addSubview(backgroundView)
+        let image = UIImage(named: images[0])
+        backgroundView.image = image
+        
+        foregroundView = CompareView(frame:view.bounds)
+        backgroundScrollView.addSubview(foregroundView)
+        foregroundView.image = UIImage(named: images[1])
+        
+        scrollView = UIScrollView(frame: view.bounds)
+        backgroundScrollView.addSubview(scrollView)
+        scrollView.delegate = self
+        scrollView.pagingEnabled = true
+        scrollView.contentSize = CGSizeMake(view.bounds.size.width*CGFloat(images.count), view.bounds.size.height)
+        scrollView.backgroundColor = UIColor.clearColor()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.bounces = false
+        foregroundView.leftOffset = view.bounds.size.width
+    }
+    
+    var lastIndex:Int = 0
+    var lastOffset:CGFloat = 0
+    
+    enum Direction {
+        case Forward
+        case Backward
+    }
+    
+    var scrollDirection:Direction = .Forward
+    
+    func scrollViewDidScroll(aScrollView: UIScrollView) {
+        
+        if aScrollView == scrollView {
+            let offset = scrollView.contentOffset.x
+            
+            scrollDirection = offset - lastOffset > 0 ? .Forward : .Backward
+            
+            foregroundView.leftOffset = view.bounds.size.width - offset % view.bounds.size.width
+            foregroundView.setNeedsDisplay()
+            
+            let index = Int(offset / view.bounds.size.width)
+            if index != lastIndex {
+                updateViewsWithImageIndex(index, forward: scrollDirection == .Forward)
+                lastIndex = index
+            }
+            
+            lastOffset = offset
+        }
+        else {
+            
+            if (lastIndex == 0 && backgroundScrollView.contentOffset.x > 0) ||
+            (lastIndex == images.count-1 && backgroundScrollView.contentOffset.x < 0)
+            {
+                backgroundScrollView.contentOffset = CGPointZero
+                
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func updateViewsWithImageIndex(index:Int, forward:Bool) {
+        
+        backgroundView.image = UIImage(named: images[index])
+        
+//        check bounds and set foreground image
+        if index + 1 < images.count {
+            foregroundView.image = UIImage(named: images[index+1])
+        }
+        if forward == true {
+            foregroundView.leftOffset = view.bounds.size.width
+        }
+        else {
+            foregroundView.leftOffset = 0
+        }
+    }
+}
+
